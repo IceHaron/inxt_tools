a6256d3c61436527032ed1ebe9a1b022cd990617
IceHaron <ice_haron@mail.ru>
2014-04-01 10:55:57 +0400

    404 на несуществующие файлы

--- index.php
+++ index.php
@@ -204,7 +204,8 @@ if (!$GAMINAS['isfile']) {																											// Если обращае�
 	if (!isset($GAMINAS['notemplate'])) INCLUDE_ONCE('html/index.html');					// Ну и подгружаем макет, конечно же
 	
 } else {																																				// Если же обращение идет непосредственно к файлу
-	INCLUDE(trim($_SERVER['REQUEST_URI'], '/'));
+	header('HTTP/1.0 404 Not Found');
+	// INCLUDE(trim($_SERVER['REQUEST_URI'], '/'));
 	// echo 'Nonono, David Blaine!';
 }
 ?>
